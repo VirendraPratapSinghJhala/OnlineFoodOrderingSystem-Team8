b958d18e5d8d25e37949c7a186d7a4765cabd998
Mehul Jain <mehuljain.it20@gmail.com>
2020-05-16 20:34:30 +0530

    Added Some Namespaces

--- OnlineFoodOrderingSystem/Controllers/CustomerController.cs
+++ OnlineFoodOrderingSystem/Controllers/CustomerController.cs
@@ -1,9 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using OnlineFoodOrderingSystem.ExceptionLayer;
 using OnlineFoodOrderingSystem.Models;
 using OnlineFoodOrderingSystem.Services;
-using System.Collections.Generic;
-using System.Web.Http;
+
 
 namespace OnlineFoodOrderingSystem.Controllers
 {
